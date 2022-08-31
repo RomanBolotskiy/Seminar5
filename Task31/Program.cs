4f7964b77088ab93805e3bd79a1779792a1903cb
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-31 22:31:15 +0300

    Задача 31

---
+++ Task31/Program.cs
@@ -0,0 +1,25 @@
+﻿int [] GetArray(int size, int minValue, int maxValue)
+{
+    int [] res = new int [size];
+
+    for (int i=0; i < size; i++)
+    {
+        res [i] = new Random().Next(minValue, maxValue +1);
+    }
+    return res;
+}
+
+int [] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(" ", array));
+
+int PositiveSum = 0;
+int NegativeSum = 0;
+
+
+foreach (int el in array)
+{
+    PositiveSum += el > 0 ? el : 0;
+    NegativeSum += el > 0 ? el :0;
+}
+
+Console.WriteLine ($"PositiveSum {PositiveSum}, NegativeSum {NegativeSum}");
