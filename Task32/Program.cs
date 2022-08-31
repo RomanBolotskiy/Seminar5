d79d3fa1a2ad8125fd39826943b598a5473c7e3c
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-31 22:32:01 +0300

    Реверс числа

--- Task32/Program.cs
+++ Task32/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] GetArray (int size, int minValue, int maxValue)
+{
+    int [] res = new int [size];
+
+    for (int i=0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue +1);
+    }
+    return res;
+}
+
+int [] revers (int [] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] *= -1;
+    }
+    return mas;
+}
+
+
+
+int [] array = GetArray (5, -10, 11);
+Console.WriteLine(String.Join(" ", array));
+
+Console.WriteLine(String.Join(" ", revers(array)));
+
+
