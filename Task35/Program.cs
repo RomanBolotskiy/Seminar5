07a15fc539cb50cf530d8e56444cc88771a90faa
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-09-01 22:07:16 +0300

    Суммф нечетных элементов массива

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] GetArray (int size, int minValue, int maxValue)
+{
+    int [] res = new int [size];
+
+    for (int i=0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue +1);
+    }
+    return res;
+}
+
+int check(int[] mas)
+{
+    int count = 0;
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if ((10 <=mas[i]) && (100>mas[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] array = GetArray(123, 0, 200);
+Console.WriteLine (String.Join(" ", array));
+Console.WriteLine ($" {check (array)}");
+
